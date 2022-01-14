b966bf0f354ec34be73a456c9717be66002ba17a
Eze Sunday Eze <mailsforeze@gmail.com>
2022-01-14 14:30:00 +0100

    add lottery contract creator

--- index.sol
+++ index.sol
@@ -2,43 +2,61 @@
 
 pragma solidity ^0.8.0;
 
-contract Lottery{
+contract Lottery {
     address payable[] public players;
     address public manager;
 
-    constructor(){
-        manager = msg.sender;
+    constructor(address eoa) {
+        manager = eoa;
     }
-    
-    modifier onlyManager{
+
+    modifier onlyManager() {
         require(msg.sender == manager);
         _;
     }
 
-    receive() external payable{
+    receive() external payable {
         require(msg.value == 0.1 ether);
-        players.push(payable (msg.sender));
+        players.push(payable(msg.sender));
     }
 
-    function getBalance() public onlyManager view returns(uint){
+    function getBalance() public view onlyManager returns (uint256) {
         return address(this).balance;
     }
 
-    function random() public view returns(uint){
-        return uint(keccak256(abi.encodePacked(block.difficulty, block.timestamp, players.length)));
+    function random() public view returns (uint256) {
+        return
+            uint256(
+                keccak256(
+                    abi.encodePacked(
+                        block.difficulty,
+                        block.timestamp,
+                        players.length
+                    )
+                )
+            );
     }
 
     error lotteryError(string _error);
 
     function pickWinner() public onlyManager {
-        
-        if(players.length < 3){
+        if (players.length < 3) {
             revert lotteryError("Not enough players");
         }
-        uint winner = random() % players.length;
+        uint256 winner = random() % players.length;
         players[winner].transfer(getBalance());
 
         players = new address payable[](0);
     }
+}
 
+
+// A simple lottery contract that creates a new lottery contract
+contract LotteryCreator {
+    Lottery[] public lotteries;
+
+    function createLottery() public payable {
+        Lottery lottery = new Lottery(msg.sender);
+        lotteries.push(lottery);
+    }
 }
